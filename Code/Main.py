4b44b4e1ddc478825afc1d3e46f9a5c0ab7170a6
madly9 <madly9@yahoo.com>
2019-04-28 23:47:08 -0400

    Update Main.py
    Added loss over time graph, rearranged some code

--- Code/Main.py
+++ Code/Main.py
@@ -12,11 +12,10 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
 import torch.optim as optim
+import torchvision.transforms as transforms
+from sklearn.metrics import confusion_matrix
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-start_time = time.time()
-#----------------------------------------------------------------------------------------------------
+#%%
 # Initial data sizes
 input_size = 131072       #Number of inputs (splited because of the LSTM model)
 hidden_size1 = 500      #Number of neurons
@@ -26,12 +25,14 @@ num_epochs = 30     #Number of epochs
 batch_size = 100       #Number of audio clips to ran through 1 iteration
 learning_rate = 0.001
 confusion_m = np.zeros((8,8))
-#----------------------------------------------------------------------------------------------------
+
+#%%
 #Classes
 
-classes = ['clarinet', 'distorted electric guitar', 'female singer', 'flute', 'piano', 'tenor saxophone', 'trumpet', 'violin']
+classes = ['clarinet', 'distorted electric guitar', 'female singer', 'flute', 
+           'piano', 'tenor saxophone', 'trumpet', 'violin']
 
-#----------------------------------------------------------------------------------------------------
+#%%
 #Spit data into test, train, validation sets
 
 print("Loading CSV...")
@@ -47,17 +48,15 @@ test = Medley.iloc[0:12236]
 test.index = range(len(test.index))
 
 print("Number of audios=", Medley.shape[0], "  Number of classes=", len(Medley.instrument.unique()))
+print()
 print(Medley.instrument.unique())
 
 
-print("Train set size: ")
-print(len(train))
-print("Validation set size: ")
-print(len(validation))
-print("Test set size: ")
-print(len(test))
+print("Train set size: ", len(train))
+print("Validation set size: ", len(validation))
+print("Test set size: ", len(test))
 
-#---------------------------------------------------------------------------------------------------
+#%%
 #Load audio file linked to the uuid
 def full_name(file):
     correspding_row = Medley.loc[Medley['uuid4'] == file].iloc[0]
@@ -70,13 +69,15 @@ def full_name(file):
 
 def load_file(file):
     file_name = full_name(file)
-    path = '/home/ubuntu/Machine-Learning/Medley-solos-DB/'
+    path = '/home/ubuntu/Final-Project-Group1/Medley-solos-DB/'
     parts = [path, file_name]
     s = ''
     link = s.join(parts)
     return link
 
-#---------------------------------------------------------------------------------------------------
+#%%
+
+#%%
 #Process Dataset
 
 class MedleyDataset(Dataset):
@@ -102,11 +103,16 @@ class MedleyDataset(Dataset):
             sample = self.transform(sample)
 
         return sample
-
+#%%
+transform = transforms.Compose([transforms.ToTensor(), 
+                                transforms.Normalize((0.5,), (0.5,))])
+#%%
+                                
 audio_dataset_train = MedleyDataset(dataset_csv= train)
 audio_dataset_validation = MedleyDataset(dataset_csv= validation)
 audio_dataset_test = MedleyDataset(dataset_csv= test)
-#----------------------------------------------------------------------------------------------------
+
+#%%
 #LSTM Model
 
 class Net(nn.Module):
@@ -121,18 +127,20 @@ class Net(nn.Module):
         s = x.shape[0]
         #print(s)
         x = x.reshape(s, 1, input_size)
-        out, states = self.LSTM1(x)
-        out, states = self.LSTM2(out)
+        out, states = self.LSTM1(x).cuda()
+        out, states = self.LSTM2(out).cuda()
         #print("Afterlstm")
         #print(out.shape)
         out = out[:,0,:]
         out = self.lstm2tag(out)
         return out
 
-#----------------------------------------------------------------------------------------------------
+#%%
 #Train LSTM
 
 model = Net(input_size, hidden_size1, hidden_size2, num_classes)
+
+#%%
 criterion = nn.CrossEntropyLoss()
 
 #optimizer = torch.optim.Adadelta(model.parameters(), rho = 0.8, eps = 1e-6, lr=learning_rate)
@@ -143,6 +151,13 @@ train_loader = torch.utils.data.DataLoader(dataset = audio_dataset_train, batch_
 validation_loader = torch.utils.data.DataLoader(dataset = audio_dataset_validation, batch_size = batch_size, shuffle = False)
 test_loader = torch.utils.data.DataLoader(dataset = audio_dataset_test, batch_size = batch_size, shuffle = False)
 
+#%%
+start_time = time.time()
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+epochs = np.array([])
+loss_index = np.array([])
 
 for epoch in range(num_epochs):
     for i, data in enumerate(test_loader):
@@ -168,15 +183,30 @@ for epoch in range(num_epochs):
         if (i + 1) % 10 == 0:
             print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
                   % (epoch + 1, num_epochs, i + 1, len(test) // batch_size, loss.data[0]))
+    
+    epochs = np.append(epochs, epoch)
+    loss_index = np.append(loss_index, loss.item())
 
-#----------------------------------------------------------------------------------------------------
+end_time = time.time()
+elapsed_time = end_time - start_time
+print("Elapsed time:", elapsed_time)
+
+#%%
+
+plt.figure(figsize = (12,8))
+plt.plot(epochs, loss_index, 'r')
+plt.xlabel("Epoch", fontsize = 14)
+plt.ylabel("Performance Index", fontsize = 14)
+plt.title("Performance Index Over Time", fontsize = 20)
+plt.show()
+
+#%%
 #Test accuracy on validation set
 
 correct = 0
 total = 0
 
 for i, data in enumerate(validation_loader):
-    print(i)
     audios = data['audio']
     labels = data['label']
 
@@ -191,29 +221,27 @@ for i, data in enumerate(validation_loader):
     _, predicted = torch.max(outputs.data, 1)
 
     total += labels.size(0)
-    correct += (predicted == labels).sum()
+    #correct += (predicted == labels).sum()
+    for j in range(len(predicted)):
+        if predicted[j] == labels[j]: correct += 1
     # print(total)
     # print(correct)
 
 
-    #confusion matrix
-    #from sklearn.metrics import confusion_matrix
-
-    #results = confusion_matrix(labels, predicted)
-
-    #confusion_m = confusion_m + results
-
-
+#Confusion matrix
+results = confusion_matrix(labels, predicted)
+confusion_m = confusion_m + results
 
 print(confusion_m)
 print('Accuracy of the network on the 3494 validation audio clips: %d %%' % (100 * correct / total))
-# --------------------------------------------------------------------------------------------
+
+#%%
 #Test accuracy of each class on Validation set
 
 class_correct = list(0. for i in range(8))
 class_total = list(0. for i in range(8))
 
-for data in validation_loader:
+for data in test_loader:
     audios = data['audio']
     labels = data['label']
 
@@ -235,16 +263,13 @@ for data in validation_loader:
         class_correct[label] += c[i]
         class_total[label] += 1
 
-# --------------------------------------------------------------------------------------------
+#%%
 
 for i in range(8):
     if (class_total[i] == 0):
         print('Accuracy of %5s : %2d %%' % (classes[i], 0))
     else:
         print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
-# --------------------------------------------------------------------------------------------
-torch.save(model.state_dict(), 'model.pkl')
-print("--- %s seconds ---" % (time.time() - start_time))
-                  
-                  
 
+#%%
+torch.save(model.state_dict(), 'model_time.pkl')
