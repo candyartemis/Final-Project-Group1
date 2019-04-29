7fa2faf074e81d7cea648426531baeff140571e7
candyartemis <50033146+candyartemis@users.noreply.github.com>
2019-04-28 22:31:14 -0400

    Update main(frequency).py

--- Code/main(frequency).py
+++ Code/main(frequency).py
@@ -1,33 +1,24 @@
-#This file doesn't use the data loader py file yet. It runs quite slow!
-
+#The commented part is the same a the music_data_
 import numpy as np
 import pandas as pd
-from scipy.io import wavfile
 from torch.autograd import Variable
 import time
 import librosa
-from tempfile import TemporaryFile
-import IPython.display as ipd
-import matplotlib.pyplot as plt
-import os, glob
 
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-import torch.nn.functional as F
-import torch.optim as optim
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 start_time = time.time()
 # ----------------------------------------------------------------------------------------------------
 # Initial data sizes
-# input_size = 131072       #Number of inputs (splited because of the LSTM model)
 input_size = 42  # Number of inputs (splited because of the LSTM model) using Librosa
 hidden_size1 = 500  # Number of neurons
 hidden_size2 = 30  # Number of neurons
 num_classes = 8  # 8 classes/labels
-num_epochs = 10  # Number of epochs
+num_epochs = 100  # Number of epochs
 batch_size = 100  # Number of audio clips to ran through 1 iteration
 learning_rate = 0.001
 confusion_m = np.zeros((8, 8))
@@ -45,8 +36,6 @@ Medley = pd.read_csv("Medley-solos-DB_metadata.csv")
 
 train = Medley.iloc[12236:18077]
 train.index = range(len(train.index))
-print(train)
-print(train.shape)
 
 validation = Medley.iloc[18077:]
 validation.index = range(len(validation.index))
@@ -64,9 +53,12 @@ print(len(validation))
 print("Test set size: ")
 print(len(test))
 
-
-# ---------------------------------------------------------------------------------------------------
-# Load audio file linked to the uuid
+#---------------------------------------------------------------------------------------------------
+#Get the nparray name
+#def full_name():
+"""""
+#---------------------------------------------------------------------------------------------------
+#Load audio file linked to the uuid
 def full_name(file):
     correspding_row = Medley.loc[Medley['uuid4'] == file].iloc[0]
     subset = str(correspding_row.loc['subset'])
@@ -76,7 +68,6 @@ def full_name(file):
     file_name = s.join(parts)
     return file_name
 
-
 def find_file(file):
     file_name = full_name(file)
     path = '/home/ubuntu/Machine-Learning/Medley-solos-DB/'
@@ -84,6 +75,48 @@ def find_file(file):
     s = ''
     link = s.join(parts)
     return link
+
+#---------------------------------------------------------------------------------------------------
+#Generating music data matrix in frequency domain using librosa
+
+def get_music_features(dataset):
+    timeseries_length = 3
+    audio = np.zeros((len(dataset), timeseries_length, 14), dtype=np.float64)
+
+    for i in range(len(dataset)):
+        row = dataset.loc[i]
+        uuid4_name = str(row.loc['uuid4'])
+        link = find_file(uuid4_name)
+
+        y, sr = librosa.load(link)
+
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+        spectral_center = librosa.feature.spectral_centroid(y=y, sr=sr)
+
+        audio[i, :, 0:13] = mfcc.T[0:timeseries_length, :]
+        audio[i, :, 13:14] = spectral_center.T[0:timeseries_length, :]
+
+        if ((i + 1) % 100 == 0):
+            print("Extracted features audio clip %i of %i." % (i + 1, len(dataset)))
+
+    return audio
+
+train_audio_data = get_music_features(train)
+validation_audio_data = get_music_features(validation)
+test_audio_data = get_music_features(test)
+
+np.save('train_audio_data.npy', train_audio_data)
+np.save('validation_audio_data.npy', validation_audio_data)
+np.save('test_audio_data.npy', test_audio_data)
+"""
+# ---------------------------------------------------------------------------------------------------
+# load npy music feature matrix data
+train_audio_data = np.load('train_audio_data.npy')
+validation_audio_data = np.load('validation_audio_data.npy')
+#test_audio_data = np.load('test_audio_data.npy')
+
+
+# test_audio_data = np.load('test_audio_data.npy')
 
 
 # ---------------------------------------------------------------------------------------------------
@@ -101,18 +134,16 @@ class MedleyDataset(Dataset):
         uuid4 = self.dataset_frame.iloc[index, 4]
         instrument_list = self.dataset_frame.iloc[index, 2]
         instrument_id = int(instrument_list)
-        link = find_file(uuid4)
+        set = self.dataset_frame
+        if (set.shape == train.shape):
+            feature_matrix = train_audio_data
+        if (set.shape == validation.shape):
+            feature_matrix = validation_audio_data
+        if (set.shape == test.shape):
+            feature_matrix = test_audio_data
 
-        # fs, audio = wavfile.read(link)
-        timeseries_length = 3
-        audio = np.zeros((timeseries_length, 14), dtype=np.float64)
-        y, sr = librosa.load(link)
+        audio = feature_matrix[index, :, :]
 
-        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
-        spectral_center = librosa.feature.spectral_centroid(y=y, sr=sr)
-
-        audio[:, 0:13] = mfcc.T[0:timeseries_length, :]
-        audio[:, 13:14] = spectral_center.T[0:timeseries_length, :]
         audio = audio.astype('float')
 
         sample = {'audio': audio, 'label': instrument_id}
@@ -139,14 +170,11 @@ class Net(nn.Module):
         self.lstm2tag = nn.Linear(hidden_size1, num_classes).cuda()
 
     def forward(self, x):
-        print(x.shape)
+        #print(x.shape)
         s = x.shape[0]
-        # print(s)
         x = x.reshape(s, 1, input_size)
         out, states = self.LSTM1(x)
         # out, states = self.LSTM2(out)
-        # print("Afterlstm")
-        # print(out.shape)
         out = out[:, 0, :]
         out = self.lstm2tag(out)
         return out
@@ -165,7 +193,7 @@ optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate, betas=
 
 train_loader = torch.utils.data.DataLoader(dataset=audio_dataset_train, batch_size=batch_size, shuffle=True)
 validation_loader = torch.utils.data.DataLoader(dataset=audio_dataset_validation, batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=audio_dataset_test, batch_size=batch_size, shuffle=True)
+# test_loader = torch.utils.data.DataLoader(dataset = dataset=audio_dataset_test, batch_size = batch_size, shuffle = True)
 
 for epoch in range(num_epochs):
     for i, data in enumerate(train_loader):
